68d0f6342b6f5888a6173c7d225d7bedeb7b07e8
qquasar <qquasar@abv.bg>
2013-06-14 15:41:31 +0000

    Grabbing environment vars

---
+++ environ.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; environ[i] != NULL; ++i)
+	{
+		printf("%s\n", environ[i]);
+	}
+
+	getchar();
+	return 0;
+}
